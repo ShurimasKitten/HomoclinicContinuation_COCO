ad3ae3a6834caf2facacf86026edd9140e348df0
John Bailie <john.bailie@auckland.ac.nz>
2024-11-17 19:04:13 +1300

    Fixed a bug in the hom2hom constructor

--- HomContinuationFiles/proj_hom2hom.m
+++ HomContinuationFiles/proj_hom2hom.m
@@ -1,51 +1,39 @@
 function prob = proj_hom2hom(fnPO, idx_hom, continuationSettings)
-    % PROJ_ISOL2HOM Initializes a COCO problem for homoclinic continuation using projection boundary conditions
-    %
-    % This function sets up a COCO (Continuation COntinuation) problem structure tailored for
-    % homoclinic orbit continuation. It leverages data from a periodic orbit (PO) continuation run
-    % to initialize the necessary conditions and parameters for projecting boundary conditions.
+    % PROJ_ISOL2HOM Initializes a COCO problem for homoclinic continuation
+    % from a previously computed hom.orb file. 
     %
     % ARGS:
-    %   - fnPO (string): Filename of the PO continuation run data file.
-    %   - idx_hom (integer): Index of the candidate homoclinic orbit from the PO continuation run.
+    %   - fnPO (string): Filename of a previous hom.orb continuation data file.
+    %   - idx_hom (integer): Index of the candidate homoclinic orbit.
     %   - continuationSettings (struct): Nested structure containing COCO settings, including
     %                                     continuation, correction, and collocation parameters.
     %
-    % RETURNS:
-    %   - prob (struct): COCO problem structure initialized for homoclinic continuation.
-    %
     % Example:
-    %   settings.contSettings.norm = 'some_norm';
     %   settings.contSettings.PtMX = 100;
     %   settings.corrSettings.TOL = 1e-6;
     %   settings.collSettings.NTST = 10;
     %   settings.collSettings.NCOL = 4;
-    %   prob = proj_isol2hom('po_data', 5, settings);
+    %   prob = proj_isol2hom('hom_orb', 5, settings);
     %
     
 
-    %% Initialize COCO Problem and Apply Settings
-    
-    % Create an initial (empty) COCO problem structure
+    %%% Initialize COCO Problem and Apply Settings    
     prob = coco_prob();
-    
-    % Apply the continuation settings to the COCO problem using a helper function
     prob = child_applySettings(prob, continuationSettings);
-   
-    %% Initialize Homoclinic Data from PO Continuation
-    
-    % Initialize data required for homoclinic continuation using data from the PO continuation run
+
+
+
+    %%% Initialize Homoclinic Data from Hom Continuation
     hom_data = hom2hom_ProjData(fnPO, idx_hom, continuationSettings);
    
-    %% Construct COCO Collocation and Equilibrium Problems
-    % Add an ODE collocation problem to the COCO problem structure
-    prob = ode_isol2coll(prob, 'x', hom_data.f, hom_data.t_sol, hom_data.x_sol, hom_data.p0);
+    %%% Construct COCO Collocation and Equilibrium Problems
+    prob = ode_isol2coll(prob, hom_data.hom_cid, hom_data.f, hom_data.t_sol, hom_data.x_sol, hom_data.p0);
     
     % Add an equilibrium point problem to the COCO problem structure
-    prob = ode_isol2ep(prob, 'x_ss', hom_data.f, hom_data.x_ss, hom_data.p0);
+    prob = ode_isol2ep(prob, hom_data.ep_cid, hom_data.f, hom_data.x_ss, hom_data.p0);
     
     % Initialize Phase Conditions    
-    hom_data = init_phaseData(prob, 'x', hom_data);
+    hom_data = init_phaseData(prob, hom_data);
 
     % Set Up Boundary Conditions
     prob = close_HomoclinicProjectionBC(prob, hom_data);
@@ -92,8 +80,8 @@ function prob = child_applySettings(prob, PS)
     prob = coco_set(prob, 'coll', 'bifus', 'off');
     
     %% Additional Collocation Settings
-    % prob = coco_set(prob, 'coll', 'NTSTMN', 50);    
-    % prob = coco_set(prob, 'coll', 'NTSTMX', 300);    
-    % prob = coco_set(prob, 'coll', 'TOLDEC', 1e-7);
-    % prob = coco_set(prob, 'coll', 'TOLINC', 1e-6);
+    prob = coco_set(prob, 'coll', 'NTSTMN', 80);    
+    prob = coco_set(prob, 'coll', 'NTSTMX', 600);    
+    prob = coco_set(prob, 'coll', 'TOLDEC', 1e-7);
+    prob = coco_set(prob, 'coll', 'TOLINC', 1e-6);
 end
